e41446dcb03b936910ca597efa624a949d6cb37e
ADEBAYO <atkinsonbayo01@gmail.com>
2025-12-06 19:25:09 +0000

    SQL Query

---
+++ ad_budget_and_sales.sql
@@ -0,0 +1,7 @@
+CREATE Table advertising (
+	ID INTEGER PRIMARY KEY,
+	tv_ad_budget FLOAT NOT NULL,
+	radio_ad_budget FLOAT NOT NULL,
+	newspaper_ad_budget FLOAT NOT NULL,
+	sales FLOAT NOT NULL
+);
