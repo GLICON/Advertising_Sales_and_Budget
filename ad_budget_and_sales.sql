c50fec422fab803d63d28017c35b87025bdd8d26
ADEBAYO <atkinsonbayo01@gmail.com>
2026-01-31 17:25:53 +0000

    Delete ad_budget_and_sales.sql

--- ad_budget_and_sales.sql
+++
@@ -1,7 +0,0 @@
-CREATE Table advertising (
-	ID INTEGER PRIMARY KEY,
-	tv_ad_budget FLOAT NOT NULL,
-	radio_ad_budget FLOAT NOT NULL,
-	newspaper_ad_budget FLOAT NOT NULL,
-	sales FLOAT NOT NULL
-);
