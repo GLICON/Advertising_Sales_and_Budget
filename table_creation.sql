c265248549b3d889f3dd4b55df2c7f929578dc0e
ADEBAYO <atkinsonbayo01@gmail.com>
2026-01-31 17:19:28 +0000

    created table

---
+++ table_creation.sql
@@ -0,0 +1,7 @@
+CREATE Table Advertisement_Budget_and_Sales (
+      ID INTEGER PRIMARY KEY,
+      tv_ad_budget FLOAT NOT NULL,
+      radio_ad_budget FLOAT NOT NULL,
+      newspaper_ad_budget FLOAT NOT NULL,
+      sales FLOAT NOT NULL
+);
